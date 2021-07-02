5f02930d24feeb48c8816e808a06b82e0bf93569
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-02 17:05:45 +0300

    Replace new invocation to getting bean from the context.

--- src/main/java/com/frankmoley/lil/fid/Application.java
+++ src/main/java/com/frankmoley/lil/fid/Application.java
@@ -1,17 +1,16 @@
 package com.frankmoley.lil.fid;
 
-import com.frankmoley.lil.fid.service.GreetingService;
+import com.frankmoley.lil.fid.config.ApplicationConfig;
 import com.frankmoley.lil.fid.service.OutputService;
-import com.frankmoley.lil.fid.service.TimeService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        GreetingService greetingService = new GreetingService("Hello");
-        TimeService timeService = new TimeService(true);
-        OutputService outputService = new OutputService(greetingService, timeService);
+        var context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        var outputService = context.getBean(OutputService.class);
 
-        for (int i=0;i<5;i++){
+        for (int i = 0; i < 5; i++) {
             outputService.generateOutput("Frank");
             Thread.sleep(5000);
         }
