4ced527cd753d7741f1a7d0ebd5ada054452e22c
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-03 21:27:59 +0300

    Add Logger to Logging Aspect class

--- src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -2,11 +2,15 @@ package com.frankmoley.lil.fid.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Pointcut("@annotation(Loggable)")
     public void executeLogging() {
