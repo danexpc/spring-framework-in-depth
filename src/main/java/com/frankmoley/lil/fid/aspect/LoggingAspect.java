c3bfb2945f2ec153b2cc27c84c362eb3c1a4de2a
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-03 21:38:17 +0300

    Add before aspect that logs method's name and parameters

--- src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -1,10 +1,14 @@
 package com.frankmoley.lil.fid.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
@@ -14,5 +18,19 @@ public class LoggingAspect {
 
     @Pointcut("@annotation(Loggable)")
     public void executeLogging() {
+    }
+
+    @Before("executeLogging()")
+    public void logMethodCall(JoinPoint joinPoint) {
+        var message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName());
+        var args = joinPoint.getArgs();
+        if (args != null && args.length > 0) {
+            message.append(" args[ | ");
+            Arrays.asList(args).forEach(arg -> message.append(arg).append(" | "));
+            message.append("]");
+        }
+        var logMessage = message.toString();
+        LOGGER.info(logMessage);
     }
 }
