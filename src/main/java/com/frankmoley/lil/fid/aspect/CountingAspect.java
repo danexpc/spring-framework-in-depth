ed0874591c30cabd0da9a6543d2f160968733f3e
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-03 23:18:57 +0300

    Replace static count integer field to Map<String, Integer> which maps method name to count number

--- src/main/java/com/frankmoley/lil/fid/aspect/CountingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/aspect/CountingAspect.java
@@ -8,23 +8,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 @Aspect
 public class CountingAspect {
 
-    private static int count;
     private static final Logger LOGGER = LoggerFactory.getLogger(CountingAspect.class);
+
+    private static final Map<String, Integer> countingMap = new HashMap<>();
 
     @Pointcut("@annotation(Countable)")
     public void executeCounting() {}
 
     @Before("executeCounting()")
     public void countMethodCall(JoinPoint joinPoint) {
-        var message = new StringBuilder("Method: ");
-        message.append(joinPoint.getSignature().getName());
-        message.append(" | counter: ").append(++count);
+        var methodName = joinPoint.getSignature().getDeclaringType() + "." + joinPoint.getSignature().getName();
 
-        var logMessage = message.toString();
+        if (countingMap.containsKey(methodName)) {
+            countingMap.put(methodName, countingMap.get(methodName) + 1);
+        } else {
+            countingMap.put(methodName, 1);
+        }
+
+        var logMessage = "Method: " + methodName +
+                " | counter: " + countingMap.get(methodName);
         LOGGER.info(logMessage);
     }
 }
